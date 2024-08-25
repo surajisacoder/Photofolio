6f00740a06835a4ced73dd036a69cbca1f5a6472
Suraj Kumar <146581639+surajisacoder@users.noreply.github.com>
2024-08-25 22:32:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import styles from "./App.module.css";
-
-// components imports
 import { Navbar } from "./components/navbar/Navbar";
 import { AlbumsList } from "./components/albumsList/AlbumsList";
-
-// react toasts
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-// import { toast } from "react-toastify";
 
-function App() {
+function App() { 
   return (
     <div className={styles.App}>
       <ToastContainer />
